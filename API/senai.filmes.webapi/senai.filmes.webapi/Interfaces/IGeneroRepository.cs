f0599492c97a6a2011b3ed1e1b9a32cbce7ef66c
Carlos Eduardo Barbosa Gonçalves <43442346851@info.local>
2020-02-14 11:05:23 -0200

    Inicio das aulas de API

---
+++ API/senai.filmes.webapi/senai.filmes.webapi/Interfaces/IGeneroRepository.cs
@@ -0,0 +1,18 @@
+﻿using senai.filmes.webapi.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace senai.filmes.webapi.Interfaces
+{
+    interface IGeneroRepository
+    {
+
+        /// <summary>
+        /// Lista todos os generos
+        /// </summary>
+        /// <returns>Retorna uma lista de generos</returns>
+        List<GeneroDomain> Listar();
+    }
+}
